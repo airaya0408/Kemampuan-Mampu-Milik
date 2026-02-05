cb38a8bc6c455a4f63234ef8526942e7d9c60461
airaya0408 <aimi2022@gmail.com>
2026-02-05 12:15:51 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    extensions: ['.js', '.ts', '.jsx', '.tsx']
+    alias: {
+      // Memastikan import relatif sentiasa berfungsi dari root
+      '@': '/.'
+    }
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false
   }
 });
